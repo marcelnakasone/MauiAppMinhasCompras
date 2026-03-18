d4e6a622b2a087ff92871dbda0ab71c10212487b
marce <marcelmassahiro@hotmail.com>
2026-03-18 12:11:05 -0300

    corrigindo descrição e preço que apareciam em brancos

--- MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
@@ -18,15 +18,16 @@ public partial class NovoProduto : ContentPage
 			{
 				Descricao = txt_descricao.Text,
 				Quantidade = Convert.ToDouble(txt_quantidade.Text),
-				 Preco = Convert.ToDouble(txt_preco.Text)
+				Preco = Convert.ToDouble(txt_preco.Text)
 			};
 
 			await App.Db.Insert(p);
-			await DisplayAlert("Sucesso!", "Registo Inserido", "Ok");
+			await DisplayAlertAsync("Sucesso!", "Registo Inserido", "Ok");
+			await Navigation.PopAsync();
 
-		} catch(Exception ex)
+        } catch(Exception ex)
 		{
-			await DisplayAlert("Ops", ex.Message, "OK");
+			await DisplayAlertAsync("Ops", ex.Message, "OK");
 		}
     }
 }
